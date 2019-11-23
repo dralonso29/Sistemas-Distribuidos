6792025c08f74081fd0252279d74bdfcbb368674
David <d.rodriguezalon@alumnos.urjc.es>
2019-11-23 16:36:30 +0100

    Esperamos a todas las gorutinas de los coches

--- practicas/2/p2.go
+++ practicas/2/p2.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"sync"
 )
 
 
-const NCOCHES = 10 //numero total de coches
+const NCOCHES = 4 //numero total de coches
 const MINSEC = 1
 const MAXSEC = 3
 
@@ -37,25 +38,28 @@ func sideSelector(prob int)  string{
 	return "west"
 }
 
-func carRunning(id int, side string)  {
+func carRunning(id int, side string, n *sync.WaitGroup)  {
+	defer n.Done()
 	fmt.Println("Coche ",id," en el lado ", side)
 }
 
 func carGenerator()  {
 	prob := rand.Intn(100) + 1
+	var n sync.WaitGroup
 	for i := 0; i < NCOCHES; i++ {
 		side := sideSelector(prob)
-		carRunning(i+1, side) // goroutines
+		n.Add(1)
+		go carRunning(i+1, side, &n) // goroutines
 		time.Sleep(time.Duration(randomTime(MAXSEC)) * time.Second) //we'll wait random time between MINSEC and MAXSEC
 	}
+	n.Wait()
 }
 
 //!+main
 func main() {
 	rand.Seed(time.Now().UnixNano()) //importante que sino en todas las ejecuciones nos va a salir el mismo numero
 	fmt.Println("Comienza el programa")
-	//aqui iria la gorutina que debe esperar a todos los coches
-	//y aqui se deberia hacer el carGenerator
 	carGenerator()
+	fmt.Println("Termina el programa")
 }
 //!-main
